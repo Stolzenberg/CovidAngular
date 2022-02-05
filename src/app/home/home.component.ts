0aa3022ba8a73c432a66ab31f1a79e6be38aaadd
Stolzenberg <timstolzenberg@outlook.de>
2022-02-05 20:57:05 +0100

    Add little loading bar and make it responsive for mobile

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,44 +9,7 @@ import { NewsGrabberService } from 'src/services/news-grabber.service';
 })
 export class HomeComponent implements OnInit {
 
-    public articles: Article[] = [
-        {
-            header: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ",
-            imgUrl: "https://media.gettyimages.com/photos/newspaper-1945-headline-declaring-end-of-wwii-picture-id458716395?s=612x612",
-            link: "https://www.google.com/",
-            source: "Google"
-        },
-        {
-            header: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ",
-            imgUrl: "https://media.gettyimages.com/photos/newspaper-1945-headline-declaring-end-of-wwii-picture-id458716395?s=612x612",
-            link: "https://www.google.com/",
-            source: "Google"
-        },
-        {
-            header: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ",
-            imgUrl: "https://media.gettyimages.com/photos/newspaper-1945-headline-declaring-end-of-wwii-picture-id458716395?s=612x612",
-            link: "https://www.google.com/",
-            source: "Google"
-        },
-        {
-            header: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ",
-            imgUrl: "https://media.gettyimages.com/photos/newspaper-1945-headline-declaring-end-of-wwii-picture-id458716395?s=612x612",
-            link: "https://www.google.com/",
-            source: "Google"
-        },
-        {
-            header: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ",
-            imgUrl: "https://media.gettyimages.com/photos/newspaper-1945-headline-declaring-end-of-wwii-picture-id458716395?s=612x612",
-            link: "https://www.google.com/",
-            source: "Google"
-        },
-        {
-            header: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. ",
-            imgUrl: "https://media.gettyimages.com/photos/newspaper-1945-headline-declaring-end-of-wwii-picture-id458716395?s=612x612",
-            link: "https://www.google.com/",
-            source: "Google"
-        }
-    ];
+    public articles: Article[] = [];
 
     constructor(private newsGrabberService: NewsGrabberService) { }
 
